be8f7a3a24e72f13f82d014dc8e6cbfa2d1f2b05
Lei <lynnl.yet@gmail.com>
2018-09-10 23:18:57 +0800

    Make this kext as a kext library

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <mach/mach_types.h>
+#include <libkern/libkern.h>
+
+#ifdef __makefile__
+extern kern_return_t _start(kmod_info_t *, void *);
+extern kern_return_t _stop(kmod_info_t *, void *);
+
+/* Will expand name if it's a macro */
+#define KMOD_EXPLICIT_DECL2(name, ver, start, stop) \
+	KMOD_EXPLICIT_DECL(name, ver, start, stop)
+
+KMOD_EXPLICIT_DECL2(BUNDLEID, KEXTBUILD_S, _start, _stop)
+/* This kext have no standard entry/exitry  acts as kext library */
+__private_extern__ kmod_start_func_t *_realmain = NULL;
+__private_extern__ kmod_stop_func_t *_antimain = NULL;
+__private_extern__ int _kext_apple_cc = __APPLE_CC__;
+#endif
+
